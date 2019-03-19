f6be6995948fa436fe61e1b54c5d22dd83ca7e98
Harvey Herela <herelah@uw.edu>
2019-03-18 21:28:48 -0700

    Assignment 1: flask mailroom

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,7 +15,28 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+def to_float(flt):
+    try:
+        return float(flt)
+    except ValueError:
+        return 0.0
+
+
+@app.route('/add_donation', methods=['GET', 'POST'])
+def add_donation():
+    if request.method == 'GET':
+        # User clicked the "add donation" link to get here
+        donors = Donor.select()
+        return render_template('add_donation.jinja2', donors=donors)
+    elif request.method == 'POST':
+        # User pressed the 'add' button
+        donor_name = Donor.select().where(Donor.name == request.form['name']).get()
+        donation_amount = to_float(request.form['donation'])
+        if donation_amount > 0.0:
+            Donation(donor=donor_name, value=donation_amount).save()
+        return redirect(url_for('all'))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
